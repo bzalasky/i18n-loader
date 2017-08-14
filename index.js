a4837d8e35b62777f178c6147d91dff016095d28
Benjamin Zalasky <benjamin.zalasky@lookout.com>
2017-08-14 12:22:46 -0400

    Return interpolated module from i18n JSON

---
+++ index.js
@@ -0,0 +1,32 @@
+const loaderUtils = require('loader-utils');
+const grunt = require('grunt');
+
+module.exports = source => {
+  this.cacheable && this.cacheable();
+
+  let sourceFileName = loaderUtils.getRemainingRequest(this);
+
+  try {
+    let lang = /([^/]{2})\.yml/.exec(sourceFileName)[1];
+    let dest = `./public/${lang}.js`;
+
+    let momentLocale;
+
+    if (lang !== 'en') {
+      momentLocale = grunt.file.read(
+        `./node_modules/moment/locale/${lang}.js`,
+        {encoding: 'utf-8'}
+      );
+    }
+
+    let content = grunt.template.process(
+      grunt.file.read('./public/i18n-lang.js.tpl', {encoding: 'utf-8'}),
+      {data: {translations: json, moment_locale: momentLocale}}
+    );
+
+    return content;
+
+  } catch(err) {
+    this.emitError(err);
+  }
+};
